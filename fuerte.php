ebe1da8bc8fe8b3c6b36f3c069b854f8e9b903d2
Aaron Collegeman <aaron@c21redwood.com>
2020-12-24 13:02:56 -0500

    Oops, I mean v1.0.6…

--- fuerte.php
+++ fuerte.php
@@ -3,7 +3,7 @@
  * Plugin Name: Fuerte
  * Plugin URI: https://github.com/collegeman/fuerte
  * Description: A strong password generator with no dependencies, for JavaScript and PHP, Laravel and WordPress.
- * Version: 1.0.5
+ * Version: 1.0.6
  * Requires at least: 2.5.0
  * Requires PHP: 7.3
  * Author: Aaron Collegeman
